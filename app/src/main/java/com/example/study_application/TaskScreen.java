64f1ab759d727d16bfb09fcd1bab57b572202e79
Adrian Cruz <17162@students.mrgs.school.nz>
2021-05-29 17:58:30 +1200

    fixed the porgress bar as well as added a timer. The next steps is to link this to a txt file with the created tasks and store a value for the timer when creating a new task.

--- app/src/main/java/com/example/study_application/TaskScreen.java
+++ app/src/main/java/com/example/study_application/TaskScreen.java
@@ -3,14 +3,15 @@ package com.example.study_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class TaskScreen extends AppCompatActivity {
-    private int progres = 0;
 
-    Button increase,decrease;
+    Button StartEndTimer;
     ProgressBar timerBar;
     TextView timeBarText;
 
@@ -18,29 +19,32 @@ public class TaskScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_screen);
+        timerBar = findViewById(R.id.timerBar);
+        StartEndTimer = findViewById(R.id.StartEndTimer);
+        timeBarText = findViewById(R.id.timeBarText);
 
-        increase = findViewById(R.id.buttonIncrease);
-        decrease = findViewById(R.id.buttonDecrease);
-
-        increase.setOnClickListener(v -> {
-            if (progres <=90){
-                progres += 10;
-                updateProgressBar();
-            }
-        });
-
-        decrease.setOnClickListener(v -> {
-            if (progres >=10){
-                progres -= 10;
-                updateProgressBar();
-            }
-        });
+        StartEndTimer.setOnClickListener(v -> startTimer(60));
     }
 
-    private void updateProgressBar(){
-        Intrinsics.checkNotNullExpressionValue(timerBar, "progress_bar");
-        timerBar.setProgress(this.progres);
-        Intrinsics.checkNotNullExpressionValue(timeBarText, "text_view_progress");
-        timeBarText.setText((CharSequence)("" + this.progres + '%'));
+    public void startTimer(int minuti){
+         Object countDownTimer = new CountDownTimer(60 * minuti * 1000, 500) {
+
+            @Override
+            public void onTick(long leftTimeInMilliseconds) {
+                long seconds = leftTimeInMilliseconds / 1000;
+                timerBar.setProgress((int) seconds);
+                timeBarText.setText(String.format("%02d", seconds / 60) + ":" + String.format("%02d", seconds % 60));
+            }
+
+            @Override
+            public void onFinish() {
+                if (timeBarText.getText().equals("00:00")) {
+                    timeBarText.setText("STOP");
+                } else {
+                    timeBarText.setText("2:00");
+                    timerBar.setProgress(60 * minuti);
+                }
+            }
+        }.start();
     }
 }
