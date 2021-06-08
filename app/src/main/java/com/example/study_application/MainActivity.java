91dc9353875b0e80d0fcbc7b752b736fcce3a6d6
Adrian Cruz <17162@students.mrgs.school.nz>
2021-06-09 09:05:21 +1200

    fixed the google sign in

--- app/src/main/java/com/example/study_application/MainActivity.java
+++ app/src/main/java/com/example/study_application/MainActivity.java
@@ -2,25 +2,22 @@ package com.example.study_application;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
-    //text variables
-    EditText mEmail;
-    EditText mPassword;
-    String mEmail_string, mPassword_string;
+    int RC_SIGN_IN = 0; //declear variable
 
     //button variables
-    Button login_button;
+    SignInButton login_button;
     GoogleSignInClient mGoogleSignInClient;
 
     //firebase variables
@@ -28,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
     //different intents to go to different pages
     Intent intent;
-    Intent intents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,52 +32,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         login_button = findViewById(R.id.login_button);
         mAuth = FirebaseAuth.getInstance();
-        mEmail = findViewById(R.id.emailAddressText);
-        mPassword = findViewById(R.id.passwordText);
 
         intent = new Intent(this, HomeScreen.class);
-        intents = new Intent(this, RegisterScreen.class);
+
+        login_button.setOnClickListener(v -> {
+            if (v.getId() == R.id.login_button) {
+                signIn();
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)// these lines are from google, they directed the integration
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
     }
 
-    public void signIn(View v) {
-        attemptLogin();
+    public void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();//intent from google client
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    private void attemptLogin() {
-        mEmail_string = mEmail.getText().toString();
-        mPassword_string = mPassword.getText().toString();
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+    }
 
-        if ((!isEmailValid(mEmail_string) || (!isPasswordValid(mPassword_string)))) {
-            mPassword.setError("password is to short try again");
-            mEmail.requestFocus();
-        } else {
-            loginFirebaseUser();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) { //check if RC_SIGN_IN is correct
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult();
         }
     }
 
-    private boolean isEmailValid(String email) {
-        return email.contains("@") && email.length() > 3;
-    }
+    private void handleSignInResult() {
 
-    private boolean isPasswordValid(String password) {
-        return password.length() > 6;
-    }
-
-    private void loginFirebaseUser() {
-        mAuth.signInWithEmailAndPassword(mEmail_string, mPassword_string).addOnCompleteListener(task -> {
-            if (!task.isSuccessful()) {
-                Log.d("firebase", "User login failed");
-            } else {
-                Log.d("firebase", "User login Complete");
-                startActivity(intent);
-                overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
-            }
-        });
-    }
-
-    public void toRegisterScreen(View aView) {
-        finish();
-        startActivity(intents);
-        overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+        // Signed in successfully, show authenticated UI.
+        //updateUI(account); replaced
+        startActivity(intent);
     }
 }
