80360ae253334ffe5f2704d9730ca9db03736328
Adrian Cruz <17162@students.mrgs.school.nz>
2021-07-06 01:29:49 +1200

    made the xml for the break timer

--- app/src/main/java/com/example/study_application/BreakTimerScreen.java
+++ app/src/main/java/com/example/study_application/BreakTimerScreen.java
@@ -3,6 +3,7 @@ package com.example.study_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class BreakTimerScreen extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class BreakTimerScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_break_timer_screen);
+        getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
     }
 }
--- app/src/main/java/com/example/study_application/TaskScreen.java
+++ app/src/main/java/com/example/study_application/TaskScreen.java
@@ -249,6 +249,7 @@ public class TaskScreen extends AppCompatActivity {
     }
 
     private void startBreakTimer() {
+        // as we are using milliseconds i have to increase it by multiplying it by 1000 the 1500 originally is the 25 minute mark.
         BreakTimeLeft = 1500000;
         //creates new count down timer
         countBreakTimer = new CountDownTimer(1500000, 500) {
