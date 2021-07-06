c7f0af6eb4689079d58d4c8f2241399c43044251
17162 <17162@students.mrgs.school.nz>
2021-07-06 14:09:53 +1200

    made the code without testing. next step is to test at home

--- app/src/main/java/com/example/study_application/BreakTimerScreen.java
+++ app/src/main/java/com/example/study_application/BreakTimerScreen.java
@@ -1,22 +1,44 @@
 package com.example.study_application;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class BreakTimerScreen extends AppCompatActivity {
 
     long BreakTimeLeft;
     private CountDownTimer countBreakTimer;
+    TextView progressBreakBar;
+    Intent TaskScreen;
+    Button stopButton, startButton, cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_break_timer_screen);
+        TaskScreen = new Intent(this, TaskScreen.class);
+        stopButton = findViewById(R.id.stopButton);
+        startButton = findViewById(R.id.startButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
         getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        stopButton.setVisibility(View.INVISIBLE);
+
+        startButton.setOnClickListener(v -> {
+            startButton.setVisibility(View.GONE);
+            startBreakTimer();
+        });
+
+        cancelButton.setOnClickListener(v -> startActivity(TaskScreen));
     }
 
     private void startBreakTimer() {
@@ -28,12 +50,24 @@ public class BreakTimerScreen extends AppCompatActivity {
             @Override
             public void onTick(long leftTimeInMilliseconds) {
                 BreakTimeLeft = leftTimeInMilliseconds;
+                updateCountDownText();
             }
 
             @Override
             public void onFinish() {
                 countBreakTimer.cancel();
+                progressBreakBar.setText("Break Has Ended");
+                cancelButton.setVisibility(View.GONE);
+                stopButton.setVisibility(View.VISIBLE);
+                stopButton.setOnClickListener(v -> startActivity(TaskScreen));
             }
         }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (BreakTimeLeft / 1000) / 60;
+        int seconds = (int) (BreakTimeLeft / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        progressBreakBar.setText(timeLeftFormatted);
     }
 }
